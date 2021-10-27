3644890a8c2b2185fe7f1f9980053fd873a7d4a6
saralucia1224 <sara.carmona.7927@eam.edu.co>
2021-10-27 18:17:48 -0500

    IDAOTipoAlojamiento

---
+++ src/Definiciones/IDAOTipoAlojamiento.java
@@ -0,0 +1,18 @@
+package Definiciones;
+
+import Modelo.TipoAlojamiento;
+
+/**
+ *
+ * @author Sara Lucía
+ */
+public interface IDAOTipoAlojamiento {
+    
+    public boolean guardarTipoAlojamiento();
+    
+    public TipoAlojamiento buscarTipoAlojamiento();
+            
+    public TipoAlojamiento modificarTipoAlojamiento();
+    
+    public TipoAlojamiento listarTipoAlojamiento();
+}
